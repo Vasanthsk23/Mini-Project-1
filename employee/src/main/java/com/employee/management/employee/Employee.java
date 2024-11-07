132ce713ae44fe13acdc52cab6306d5cec349747
Vasanthsk23 <vasanthk2312@gmail.com>
2024-11-07 19:12:10 +0530

    completed-1

--- employee/src/main/java/com/employee/management/employee/Employee.java
+++ employee/src/main/java/com/employee/management/employee/Employee.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,10 +24,22 @@ public class Employee {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
     private Long id;
+
+    @NotEmpty(message = "First name cannot be empty.")
+    @Size(min = 5, max = 250)
     private String firstName;
+
     private String lastName;
+
+    @NotEmpty(message = "Email id cannot be empty.")
     private String emailId;
+
+    @NotNull(message = "Age cannot be empty.")
+    @Min(value = 18)
+    @Max(value=60)
     private int age;
+
+
     private double salary;
 
 }
--- employee/src/main/java/com/employee/management/employee/EmployeeController.java
+++ employee/src/main/java/com/employee/management/employee/EmployeeController.java
@@ -1,21 +1,33 @@
 package com.employee.management.employee;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-
+@Tag(name = "Employee", description = "Employee management APIs")
 @Controller
 public class EmployeeController {
 
     @Autowired
     EmployeeRepository employeeRepository;
 
+    @Operation(
+            summary = "Retrieve all employees",
+            description = "Get all employees. The response is html.",
+            tags = { "employees", "get" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = Employee.class), mediaType = "application/html") }),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) }) })
     @GetMapping("/")
     public String viewHomePage(Model model){
         model.addAttribute("allemplist", employeeRepository.findAll());
@@ -29,8 +41,20 @@ public class EmployeeController {
         return "add-user";
     }
 
+    @Operation(
+            summary = "Add/Update new employee",
+            description = "Add/update new employee. The response is home page html.",
+            tags = { "add/update employee", "post" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = Employee.class), mediaType = "application/html") }),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) }) })
+
     @PostMapping("/save")
-    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+    public String saveEmployee(@RequestBody @ModelAttribute("employee") @Valid Employee employee, BindingResult result) {
+        if(result.hasErrors()){
+            return "add-user";
+        }
         employeeRepository.save(employee);
         return "redirect:/";
     }
@@ -42,6 +66,14 @@ public class EmployeeController {
         return "update-user";
     }
 
+    @Operation(
+            summary = "Deletes an employee by id",
+            description = "delete an employee by id. The response is home page html.",
+            tags = { "delete an employee", "get" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = Employee.class), mediaType = "application/html") }),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) }) })
     @GetMapping("/deleteEmployee/{id}")
     public String deleteThroughId(@PathVariable(value = "id") long id) {
         employeeRepository.deleteById(id);
@@ -49,6 +81,14 @@ public class EmployeeController {
 
     }
 
+    @Operation(
+            summary = "Get an employee by id",
+            description = "Get an employee by id. The response is home page html.",
+            tags = { "get an employee by id", "get" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = Employee.class), mediaType = "application/html") }),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) }) })
     @GetMapping("/showUser/{id}")
     public String viewUser(@PathVariable(value = "id") Long id, Model model){
         Employee employee = employeeRepository.findById(id).get();
